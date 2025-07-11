40cb3b621f72116735eb436b29b67a59a196f73c
hussonaxel <aaaxel.hsson@gmail.com>
2025-07-12 01:21:29 +0200

    Amélioration de la mise en page et du style des cartes Pokémon dans le composant PokemonDatabaseCard

--- src/components/PokemonDatabaseCard.tsx
+++ src/components/PokemonDatabaseCard.tsx
@@ -14,9 +14,9 @@ export default function PokemonDatabaseCard() {
   })
 
   return (
-    <article className="grid grid-cols-6 gap-8 mx-8 my-4">
+    <article className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 md gap-8 mx-8 my-4">
       <Card>
-        <CardHeader>
+        <CardHeader className="flex flex-col items-center justify-center">
           <img
             src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
             alt="Pokemon"
@@ -24,84 +24,14 @@ export default function PokemonDatabaseCard() {
           />
           <CardTitle>Bulbasaur</CardTitle>
           <CardDescription>#001</CardDescription>
-          <CardDescription>
-            <p>GRASS</p>
-            <p>POISON</p>
+          <CardDescription className="text-center text-black flex gap-4">
+            <p className=" bg-green-500 py-1 px-2 rounded-sm">GRASS</p>
+            <p className="bg-purple-500 py-1 px-2 rounded-sm">POISON</p>
           </CardDescription>
         </CardHeader>
       </Card>
       <Card>
-        <CardHeader>
-          <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-            alt="Pokemon"
-          />
-          <CardTitle>Bulbasaur</CardTitle>
-          <CardDescription>#001</CardDescription>
-          <CardDescription>
-            <p>GRASS</p>
-            <p>POISON</p>
-          </CardDescription>
-        </CardHeader>
-      </Card>
-      <Card>
-        <CardHeader>
-          <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-            alt="Pokemon"
-          />
-          <CardTitle>Bulbasaur</CardTitle>
-          <CardDescription>#001</CardDescription>
-          <CardDescription>
-            <p>GRASS</p>
-            <p>POISON</p>
-          </CardDescription>
-        </CardHeader>
-      </Card>
-      <Card>
-        <CardHeader>
-          <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-            alt="Pokemon"
-          />
-          <CardTitle>Bulbasaur</CardTitle>
-          <CardDescription>#001</CardDescription>
-          <CardDescription>
-            <p>GRASS</p>
-            <p>POISON</p>
-          </CardDescription>
-        </CardHeader>
-      </Card>
-      <Card>
-        <CardHeader>
-          <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-            alt="Pokemon"
-          />
-          <CardTitle>Bulbasaur</CardTitle>
-          <CardDescription>#001</CardDescription>
-          <CardDescription>
-            <p>GRASS</p>
-            <p>POISON</p>
-          </CardDescription>
-        </CardHeader>
-      </Card>
-      <Card>
-        <CardHeader>
-          <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-            alt="Pokemon"
-          />
-          <CardTitle>Bulbasaur</CardTitle>
-          <CardDescription>#001</CardDescription>
-          <CardDescription>
-            <p>GRASS</p>
-            <p>POISON</p>
-          </CardDescription>
-        </CardHeader>
-      </Card>
-      <Card>
-        <CardHeader>
+        <CardHeader className="flex flex-col items-center justify-center">
           <img
             src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
             alt="Pokemon"
@@ -109,79 +39,159 @@ export default function PokemonDatabaseCard() {
           />
           <CardTitle>Bulbasaur</CardTitle>
           <CardDescription>#001</CardDescription>
-          <CardDescription>
-            <p>GRASS</p>
-            <p>POISON</p>
+          <CardDescription className="text-center text-black flex gap-4">
+            <p className=" bg-green-500 py-1 px-2 rounded-sm">GRASS</p>
+            <p className="bg-purple-500 py-1 px-2 rounded-sm">POISON</p>
           </CardDescription>
         </CardHeader>
       </Card>
       <Card>
-        <CardHeader>
+        <CardHeader className="flex flex-col items-center justify-center">
           <img
             src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
             alt="Pokemon"
+            className="w-full m-auto max-w-[150px]"
           />
           <CardTitle>Bulbasaur</CardTitle>
           <CardDescription>#001</CardDescription>
-          <CardDescription>
-            <p>GRASS</p>
-            <p>POISON</p>
+          <CardDescription className="text-center text-black flex gap-4">
+            <p className=" bg-green-500 py-1 px-2 rounded-sm">GRASS</p>
+            <p className="bg-purple-500 py-1 px-2 rounded-sm">POISON</p>
           </CardDescription>
         </CardHeader>
       </Card>
       <Card>
-        <CardHeader>
+        <CardHeader className="flex flex-col items-center justify-center">
           <img
             src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
             alt="Pokemon"
+            className="w-full m-auto max-w-[150px]"
           />
           <CardTitle>Bulbasaur</CardTitle>
           <CardDescription>#001</CardDescription>
-          <CardDescription>
-            <p>GRASS</p>
-            <p>POISON</p>
+          <CardDescription className="text-center text-black flex gap-4">
+            <p className=" bg-green-500 py-1 px-2 rounded-sm">GRASS</p>
+            <p className="bg-purple-500 py-1 px-2 rounded-sm">POISON</p>
           </CardDescription>
         </CardHeader>
       </Card>
       <Card>
-        <CardHeader>
+        <CardHeader className="flex flex-col items-center justify-center">
           <img
             src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
             alt="Pokemon"
+            className="w-full m-auto max-w-[150px]"
           />
           <CardTitle>Bulbasaur</CardTitle>
           <CardDescription>#001</CardDescription>
-          <CardDescription>
-            <p>GRASS</p>
-            <p>POISON</p>
+          <CardDescription className="text-center text-black flex gap-4">
+            <p className=" bg-green-500 py-1 px-2 rounded-sm">GRASS</p>
+            <p className="bg-purple-500 py-1 px-2 rounded-sm">POISON</p>
           </CardDescription>
         </CardHeader>
       </Card>
       <Card>
-        <CardHeader>
+        <CardHeader className="flex flex-col items-center justify-center">
           <img
             src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
             alt="Pokemon"
+            className="w-full m-auto max-w-[150px]"
           />
           <CardTitle>Bulbasaur</CardTitle>
           <CardDescription>#001</CardDescription>
-          <CardDescription>
-            <p>GRASS</p>
-            <p>POISON</p>
+          <CardDescription className="text-center text-black flex gap-4">
+            <p className=" bg-green-500 py-1 px-2 rounded-sm">GRASS</p>
+            <p className="bg-purple-500 py-1 px-2 rounded-sm">POISON</p>
           </CardDescription>
         </CardHeader>
       </Card>
       <Card>
-        <CardHeader>
+        <CardHeader className="flex flex-col items-center justify-center">
           <img
             src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
             alt="Pokemon"
+            className="w-full m-auto max-w-[150px]"
           />
           <CardTitle>Bulbasaur</CardTitle>
           <CardDescription>#001</CardDescription>
-          <CardDescription>
-            <p>GRASS</p>
-            <p>POISON</p>
+          <CardDescription className="text-center text-black flex gap-4">
+            <p className=" bg-green-500 py-1 px-2 rounded-sm">GRASS</p>
+            <p className="bg-purple-500 py-1 px-2 rounded-sm">POISON</p>
+          </CardDescription>
+        </CardHeader>
+      </Card>
+      <Card>
+        <CardHeader className="flex flex-col items-center justify-center">
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+            alt="Pokemon"
+            className="w-full m-auto max-w-[150px]"
+          />
+          <CardTitle>Bulbasaur</CardTitle>
+          <CardDescription>#001</CardDescription>
+          <CardDescription className="text-center text-black flex gap-4">
+            <p className=" bg-green-500 py-1 px-2 rounded-sm">GRASS</p>
+            <p className="bg-purple-500 py-1 px-2 rounded-sm">POISON</p>
+          </CardDescription>
+        </CardHeader>
+      </Card>
+      <Card>
+        <CardHeader className="flex flex-col items-center justify-center">
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+            alt="Pokemon"
+            className="w-full m-auto max-w-[150px]"
+          />
+          <CardTitle>Bulbasaur</CardTitle>
+          <CardDescription>#001</CardDescription>
+          <CardDescription className="text-center text-black flex gap-4">
+            <p className=" bg-green-500 py-1 px-2 rounded-sm">GRASS</p>
+            <p className="bg-purple-500 py-1 px-2 rounded-sm">POISON</p>
+          </CardDescription>
+        </CardHeader>
+      </Card>
+      <Card>
+        <CardHeader className="flex flex-col items-center justify-center">
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+            alt="Pokemon"
+            className="w-full m-auto max-w-[150px]"
+          />
+          <CardTitle>Bulbasaur</CardTitle>
+          <CardDescription>#001</CardDescription>
+          <CardDescription className="text-center text-black flex gap-4">
+            <p className=" bg-green-500 py-1 px-2 rounded-sm">GRASS</p>
+            <p className="bg-purple-500 py-1 px-2 rounded-sm">POISON</p>
+          </CardDescription>
+        </CardHeader>
+      </Card>
+      <Card>
+        <CardHeader className="flex flex-col items-center justify-center">
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+            alt="Pokemon"
+            className="w-full m-auto max-w-[150px]"
+          />
+          <CardTitle>Bulbasaur</CardTitle>
+          <CardDescription>#001</CardDescription>
+          <CardDescription className="text-center text-black flex gap-4">
+            <p className=" bg-green-500 py-1 px-2 rounded-sm">GRASS</p>
+            <p className="bg-purple-500 py-1 px-2 rounded-sm">POISON</p>
+          </CardDescription>
+        </CardHeader>
+      </Card>
+      <Card>
+        <CardHeader className="flex flex-col items-center justify-center">
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+            alt="Pokemon"
+            className="w-full m-auto max-w-[150px]"
+          />
+          <CardTitle>Bulbasaur</CardTitle>
+          <CardDescription>#001</CardDescription>
+          <CardDescription className="text-center text-black flex gap-4">
+            <p className=" bg-green-500 py-1 px-2 rounded-sm">GRASS</p>
+            <p className="bg-purple-500 py-1 px-2 rounded-sm">POISON</p>
           </CardDescription>
         </CardHeader>
       </Card>
