6c74d6f91daeabe073ae58f15ee2269309972504
axel husson <aaaxel.hsson@gmail.com>
2025-07-18 13:47:56 +0200

    style: amélioration de la mise en page et du style du composant CurrentPokemonLeft pour une meilleure réactivité et cohérence visuelle

--- src/components/currentPokemon/currentPokemonLeft.tsx
+++ src/components/currentPokemon/currentPokemonLeft.tsx
@@ -18,19 +18,19 @@ export default function CurrentPokemonLeft({
   }
   return (
     <div className="md:h-screen bg-gray-100 w-full md:w-2/5 flex flex-col items-center justify-center p-6 md:sticky top-0 overflow-y-auto">
-      <h2 className="text-7xl font-semibold text-[#1A1A1A]/33 mb-4">
+      <h2 className="text-4xl sm:text-7xl font-semibold text-[#1A1A1A]/33 mb-4">
         {pokemonJapaneseName}
       </h2>
-      <h3 className="text-5xl font-bold capitalize text-[#1A1A1A] mb-2">
+      <h3 className=" text-3xl sm:text-5xl font-bold capitalize text-[#1A1A1A] mb-2">
         {pokemonDefaultName}
       </h3>
 
       <img
         src={`/assets/static/sprites/${isShiny ? 'shiny' : 'base'}/${pokemonID}.webp`}
         alt={pokemonDefaultName}
-        className="w-2/4 h-auto my-8"
+        className="h-28 md:w-2/4 md:h-auto my-8"
       />
-      <div className="flex flex-col sm:flex-row gap-4">
+      <div className="flex flex-col md:flex-row gap-4">
         <Button
           onMouseDown={handleClick}
           className="mt-4 bg-white text-black uppercase font-semibold ring ring-gray-200  transition duration-200 active:scale-95 hover:bg-gray-100 hover:ring-gray-300"
