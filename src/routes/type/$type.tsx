786f7f81ca6e362d0334f28354c3ae821a2b329f
Husson <axel.husson@smile.fr>
2025-07-22 10:26:44 +0200

    feat: add ScrollArea component and update Tabs component for improved styling and structure

--- src/routes/type/$type.tsx
+++ src/routes/type/$type.tsx
@@ -1,13 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router'
-// import TypeBasicData from '@/components/features/types/TypeBasicData'
-import TypeDamageRelations from '@/components/features/types/TypeDamageRelations'
+import ComponentTabs436 from '@/components/features/types/TabsContainer'
 export const Route = createFileRoute('/type/$type')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
   return <>
-    {/* <TypeBasicData /> */}
-    <TypeDamageRelations />
+    <ComponentTabs436 />
   </>
 }
